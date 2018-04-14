65a2f29d336bb1fff8be91ca57cd5c2b3c287f18
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-15 00:28:08 +0200

    Initial (old) CodeWars challenges

---
+++ CodeWars/6_Multiples35.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+__author__      = jhaasdijk
+__description__ = Return the sum of all the multiples of 3 or 5 below the number
+"""
+
+def solution(number):
+    sum = 0
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i
+    return sum
+
---
+++ CodeWars/8_OppositeNumber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+__author__      = jhaasdijk
+__description__ = Given a number, return its opposite
+"""
+
+def opposite(number):
+    return -number
+
