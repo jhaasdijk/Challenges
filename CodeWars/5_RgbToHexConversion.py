50e390b6970f8ca0be2af8f2c0f3dc9f4bd4ed3a
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-16 23:42:38 +0200

    Add solution to challenge RgbToHexConversion

---
+++ CodeWars/5_RgbToHexConversion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+__author__      = jhaasdijk
+__description__ = Given 3 decimal rgb values, return the hexadecimal representation
+                  - Don't forget to properly check bounds
+"""
+
+def value(x):
+    return format(max(0, (min(x, 255))), '02X')
+
+def rgb(r, g, b):
+    return f"#{value(r)}{value(g)}{value(b)}"
+
