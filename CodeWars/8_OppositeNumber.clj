d5a5bd2170acb377f095820cf7ea9549c03a6e3c
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-18 15:36:21 +0200

    Add solution to 8_OppositeNumber written in Clojure

---
+++ CodeWars/8_OppositeNumber.clj
@@ -0,0 +1,11 @@
+
+; __author__      = jhaasdijk
+; __description__ = Given a number, return its opposite
+
+(ns opposite-number)
+
+(defn opposite [number]
+  (- number))
+
+; An example of how to use this function is: (println (opposite 3))
+
