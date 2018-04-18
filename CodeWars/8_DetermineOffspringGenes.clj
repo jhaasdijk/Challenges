0d9a10101c65a57251ff6b22158ab7a2b4c46e5d
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-18 16:06:52 +0200

    Add solution to 8_DetermineOffspringGenes written in Clojure

---
+++ CodeWars/8_DetermineOffspringGenes.clj
@@ -0,0 +1,13 @@
+
+; __author__      = jhaasdijk
+; __description__ = Determine offspring sex based on genes XX and XY chromosomes
+
+(ns clojure.chromosomes)
+
+(defn chromosome-check [sperm]
+  (if (= sperm "XX")
+    "Congratulations! You're going to have a daughter."
+    "Congratulations! You're going to have a son."))
+
+; An example of how to use this function is: (println (chromosome-check "XY"))
+
