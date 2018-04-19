d73a4267994014ad827a6b7ce558c022104854b5
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-19 13:24:24 +0200

    Fix indentation style

--- CodeWars/8_HowGoodAreYouReally.clj
+++ CodeWars/8_HowGoodAreYouReally.clj
@@ -5,7 +5,7 @@
 (ns kata.how-good-are-you)
 
 (defn better_than_average [class_points your_points]
-  (< (float (/(reduce + class_points) (count class_points))) your_points)
+    (< (float (/(reduce + class_points) (count class_points))) your_points)
 )
 
 ; An example of how to use this function is: (println (better_than_average [1 2 3 4] 1))
