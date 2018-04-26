df36aaa425e82d67359347753b12b8108193fa0d
jhaasdijk <jhaasdijk@protonmail.com>
2018-04-26 16:13:21 +0200

    Add solution to 6_HappyNumbers written in Haskell

---
+++ CodeWars/6_HappyNumbers.hs
@@ -0,0 +1,20 @@
+module IsHappy where
+
+isHappy :: Integer -> Bool
+isHappy x = checkHappy [] x
+
+checkHappy :: [Integer] -> Integer -> Bool
+checkHappy xs x
+    | h == 1 = True
+    | elem (h) xs == True = False
+    | otherwise = checkHappy (h : xs) (h)
+    where
+        h = squaredsum $ digits x
+
+digits :: Integer -> [Integer]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `mod` 10]
+
+squaredsum :: [Integer] -> Integer
+squaredsum x = sum ( map (\x -> x * x) x )
+
